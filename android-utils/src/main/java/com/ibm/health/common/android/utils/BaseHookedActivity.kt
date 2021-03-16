8ceb002cd6774c1cf11d073272b0ef24b8b6428f
Waldemar Kornewald <wkornewald@ensody.com>
2021-03-16 10:33:33 +0100

    Removed mixin-based hooks (composition/observation-based hooks are better)

--- android-utils/src/main/java/com/ibm/health/common/android/utils/BaseHookedActivity.kt
+++ android-utils/src/main/java/com/ibm/health/common/android/utils/BaseHookedActivity.kt
@@ -1,6 +1,5 @@
 package com.ibm.health.common.android.utils
 
-import android.content.Intent
 import android.view.MenuItem
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
@@ -9,18 +8,12 @@ import androidx.fragment.app.FragmentActivity
 /** Base class that comes with hook support. */
 public abstract class BaseHookedActivity(@LayoutRes contentLayoutId: Int = 0) :
     AppCompatActivity(contentLayoutId),
-    OnActivityResultHook,
     LoadingStateHook {
 
     override val isLoading: IsLoading = IsLoading()
 
     /** Helper to abstract away activity and fragment differences. */
     public open fun requireActivity(): FragmentActivity = this
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        onActivityResultHook(requestCode, resultCode, data)
-    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
--- android-utils/src/main/java/com/ibm/health/common/android/utils/BaseHookedFragment.kt
+++ android-utils/src/main/java/com/ibm/health/common/android/utils/BaseHookedFragment.kt
@@ -1,19 +1,12 @@
 package com.ibm.health.common.android.utils
 
-import android.content.Intent
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 
 /** Base class that comes with hook support. */
 public abstract class BaseHookedFragment(@LayoutRes contentLayoutId: Int = 0) :
     Fragment(contentLayoutId),
-    OnActivityResultHook,
     LoadingStateHook {
 
     override val isLoading: IsLoading = IsLoading()
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        onActivityResultHook(requestCode, resultCode, data)
-    }
 }
