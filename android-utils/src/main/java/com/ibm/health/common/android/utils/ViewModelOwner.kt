a0f2dee5359677ef2a3be24c5c484fbc2a119632
Waldemar Kornewald <wkornewald@ensody.com>
2021-03-16 08:51:08 +0100

    Cleanups

--- android-utils/src/main/java/com/ibm/health/common/android/utils/ViewModelOwner.kt
+++ android-utils/src/main/java/com/ibm/health/common/android/utils/ViewModelOwner.kt
@@ -1,14 +1,8 @@
 package com.ibm.health.common.android.utils
 
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.lifecycleScope
-import com.ensody.reactivestate.EventNotifier
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 /** Common interface for activities/fragments holding a [BaseViewModel]. */
 public interface ViewModelOwner<T : BaseEvents> : LifecycleOwner {
     public val viewModel: BaseViewModel<T>
 }
-
