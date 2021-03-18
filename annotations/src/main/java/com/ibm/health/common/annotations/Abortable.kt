6070be0095ca30defa643b6c2cddf01742157876
sjannevers <stefan.jannevers@web.de>
2021-03-18 09:40:42 +0100

    add doc to annotations module

--- annotations/src/main/java/com/ibm/health/common/annotations/Abortable.kt
+++ annotations/src/main/java/com/ibm/health/common/annotations/Abortable.kt
@@ -1,5 +1,17 @@
 package com.ibm.health.common.annotations
 
+/**
+ * This can be used as a function return value, to indicate if a process should be aborted or continued after the
+ * function. This is more meaningful than a simple boolean return value.
+ */
 public sealed class Abortable
+
+/**
+ * Return Abort to indicate that the process shall be aborted now.
+ */
 public object Abort : Abortable()
+
+/**
+ * Return Continue to indicate that the process shall be continued.
+ */
 public object Continue : Abortable()
