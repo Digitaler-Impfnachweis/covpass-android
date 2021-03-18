2277c0f78caa7fd01d0305c2e0a84e5915da71fb
Waldemar Kornewald <wkornewald@ensody.com>
2021-03-18 10:51:43 +0100

    Fixes and cleanups

--- android-utils/src/main/java/com/ibm/health/common/android/utils/ViewModelOwner.kt
+++
@@ -1,8 +0,0 @@
-package com.ibm.health.common.android.utils
-
-import androidx.lifecycle.LifecycleOwner
-
-/** Common interface for activities/fragments holding a [BaseViewModel]. */
-public interface ViewModelOwner<T : BaseEvents> : LifecycleOwner {
-    public val viewModel: BaseViewModel<T>
-}
