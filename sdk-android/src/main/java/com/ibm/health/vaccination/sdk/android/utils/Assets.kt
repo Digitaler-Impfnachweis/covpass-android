ce224e580e1fc773c8c6e5bf482fd6e8c4e10e68
Waldemar Kornewald <wkornewald@ensody.com>
2021-05-07 18:00:26 +0200

    Fix for asset access

--- sdk-android/src/main/java/com/ibm/health/vaccination/sdk/android/utils/Assets.kt
+++ sdk-android/src/main/java/com/ibm/health/vaccination/sdk/android/utils/Assets.kt
@@ -3,4 +3,4 @@ package com.ibm.health.vaccination.sdk.android.utils
 import android.app.Application
 
 internal fun Application.readTextAsset(path: String): String =
-    assets.open("backend-ca.pem").bufferedReader().use { it.readText() }
+    assets.open(path).bufferedReader().use { it.readText() }
