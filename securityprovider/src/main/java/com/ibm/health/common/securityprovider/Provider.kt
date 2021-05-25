2b66e7d909d7c9eacc8c6bb14131bd44992cffa3
Waldemar Kornewald <wkornewald@ensody.com>
2021-05-25 12:11:20 +0200

    Robustness improvement

--- securityprovider/src/main/java/com/ibm/health/common/securityprovider/Provider.kt
+++ securityprovider/src/main/java/com/ibm/health/common/securityprovider/Provider.kt
@@ -5,6 +5,7 @@
 
 package com.ibm.health.common.securityprovider
 
+import de.rki.covpass.logging.Lumber
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.conscrypt.Conscrypt
 import java.security.Security
@@ -20,9 +21,13 @@ public fun initSecurityProvider() {
 }
 
 private val lazySecurityProviderInstaller by lazy {
-    // Just in case we also integrate Bouncy Castle, disable patented algorithms
+    // Disable patented algorithms in Bouncy Castle
     System.setProperty("org.bouncycastle.ec.disable_mqv", "true")
-    Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME)
+    try {
+        Security.removeProvider("BC")
+    } catch (e: Throwable) {
+        Lumber.w { "Provider BC not found. Removal failed." }
+    }
     Security.addProvider(BouncyCastleProvider())
     Security.insertProviderAt(Conscrypt.newProvider(), 1)
 }
