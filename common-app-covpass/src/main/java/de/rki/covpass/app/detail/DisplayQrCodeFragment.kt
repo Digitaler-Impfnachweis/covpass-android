253c33e754a357e1df606a81d73187cd0d367624
Stefan Jannevers-external <stefan.jannevers@web.de>
2021-06-15 16:41:33 +0200

    BVC-1500 hide bottom sheet background (#197)
    (cherry picked from commit 4c8a1a8c9dafb5e971b01321cef5f0f988e53c50)

--- common-app-covpass/src/main/java/de/rki/covpass/app/detail/DisplayQrCodeFragment.kt
+++ common-app-covpass/src/main/java/de/rki/covpass/app/detail/DisplayQrCodeFragment.kt
@@ -5,9 +5,12 @@
 
 package de.rki.covpass.app.detail
 
+import android.animation.ValueAnimator
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.core.content.ContextCompat
 import com.ensody.reactivestate.android.autoRun
 import com.ensody.reactivestate.dispatchers
 import com.ensody.reactivestate.get
@@ -41,10 +44,41 @@ internal class DisplayQrCodeFragment : BaseBottomSheet() {
         R.string.vaccination_certificate_detail_view_qrcode_screen_action_button_title
 
     private val binding by viewBinding(DisplayQrCodeBottomsheetContentBinding::inflate)
+    private var valueAnimator: ValueAnimator? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         autoRun { updateViews(get(covpassDeps.certRepository.certs)) }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        context?.let {
+            val startColor = ContextCompat.getColor(it, android.R.color.transparent)
+            val endColor = ContextCompat.getColor(it, R.color.qr_bottomsheet_background)
+            valueAnimator = ValueAnimator.ofArgb(startColor, endColor)
+            valueAnimator?.duration = 1000
+            valueAnimator?.interpolator = LinearInterpolator()
+            val bottomSheetContainer = bottomSheetBinding.bottomSheetContainer
+            valueAnimator?.addUpdateListener {
+                valueAnimator?.let {
+                    if (isResumed) {
+                        bottomSheetContainer.setBackgroundColor(it.animatedValue as Int)
+                    }
+                }
+            }
+            valueAnimator?.start()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        valueAnimator?.removeAllUpdateListeners()
+        context?.let {
+            bottomSheetBinding.bottomSheetContainer.setBackgroundColor(
+                ContextCompat.getColor(it, android.R.color.transparent)
+            )
+        }
     }
 
     private fun updateViews(certificateList: GroupedCertificatesList) {
