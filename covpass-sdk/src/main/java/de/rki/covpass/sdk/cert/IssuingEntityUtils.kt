4ffcf08be96a47b39469e8e1168ba638a6324683
David Pucher-external <pucherd89@gmail.com>
2021-07-30 08:36:37 +0200

    Cleanup (#260)

--- covpass-sdk/src/main/java/de/rki/covpass/sdk/cert/IssuingEntityUtils.kt
+++ covpass-sdk/src/main/java/de/rki/covpass/sdk/cert/IssuingEntityUtils.kt
@@ -6,31 +6,18 @@
 package de.rki.covpass.sdk.cert
 
 import de.rki.covpass.sdk.storage.IssuingEntityRepository
-import java.math.BigInteger
-import java.security.MessageDigest
+import de.rki.covpass.sdk.utils.sha512
+import de.rki.covpass.sdk.utils.toHex
 
 public fun validateEntity(uvci: String) {
     IssuingEntityRepository.entityBlacklist.forEach { blacklistedEntitySHA512 ->
         extractEntity(uvci)?.let { entity ->
-            val entitySHA512 = getSHA512(entity)
+            val entitySHA512 = entity.sha512().toHex()
             if (blacklistedEntitySHA512 == entitySHA512) {
                 throw BlacklistedEntityException()
             }
         }
     }
-}
-
-private fun getSHA512(input: String): String {
-    val md = MessageDigest.getInstance("SHA-512")
-    val messageDigest = md.digest(input.toByteArray())
-    val no = BigInteger(1, messageDigest)
-    var hashText = no.toString(16)
-
-    while (hashText.length < 32) {
-        hashText = "0$hashText"
-    }
-
-    return hashText
 }
 
 private fun extractEntity(uvci: String): String? {
