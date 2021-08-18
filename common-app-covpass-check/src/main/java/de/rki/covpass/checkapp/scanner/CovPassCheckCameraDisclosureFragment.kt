148a487d53989adab8b457a4b0ad224bc1c11fca
David Pucher-external <pucherd89@gmail.com>
2021-08-18 16:25:06 +0200

    Added Camera disclosure for CovPass (#286)

--- common-app-covpass-check/src/main/java/de/rki/covpass/checkapp/scanner/CovPassCheckCameraDisclosureFragment.kt
+++ common-app-covpass-check/src/main/java/de/rki/covpass/checkapp/scanner/CovPassCheckCameraDisclosureFragment.kt
@@ -11,27 +11,25 @@ import com.ibm.health.common.android.utils.viewBinding
 import com.ibm.health.common.navigation.android.FragmentNav
 import com.ibm.health.common.navigation.android.findNavigator
 import de.rki.covpass.checkapp.R
-import de.rki.covpass.checkapp.databinding.CameraDisclosurePopupContentBinding
 import de.rki.covpass.commonapp.BaseBottomSheet
+import de.rki.covpass.commonapp.databinding.CameraDisclosurePopupContentBinding
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-internal class CameraDisclosureFragmentNav : FragmentNav(CameraDisclosureFragment::class)
+internal class CovPassCheckCameraDisclosureFragmentNav : FragmentNav(CovPassCheckCameraDisclosureFragment::class)
 
 /**
  * Fragment which shows a disclosure for the camera permission.
  */
-internal class CameraDisclosureFragment : BaseBottomSheet() {
+internal class CovPassCheckCameraDisclosureFragment : BaseBottomSheet() {
 
+    private val binding by viewBinding(CameraDisclosurePopupContentBinding::inflate)
     override val buttonTextRes = R.string.scan_dialog_camera_access_action_button
-
-    init {
-        viewBinding(CameraDisclosurePopupContentBinding::inflate)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bottomSheetBinding.bottomSheetTitle.text = getString(R.string.scan_dialog_camera_access_title)
+        binding.cameraDisclosureContent.text = getString(R.string.scan_dialog_camera_access_message)
     }
 
     override fun onActionButtonClicked() {
