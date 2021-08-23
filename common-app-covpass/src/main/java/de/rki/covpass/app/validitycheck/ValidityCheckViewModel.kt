243c4f265813fa0bc48bc6762213ce12d441a363
Edgar Oliveira-external <edgar.oliveira@pt.softinsa.com>
2021-08-23 09:40:55 +0100

    BVC-1740 Added error dialog for no internet connection in validity (#287)
    * Added error dialog for no internet connection in validity
    
    * Katy magic
    
    Co-authored-by: IBM <IBM>

--- common-app-covpass/src/main/java/de/rki/covpass/app/validitycheck/ValidityCheckViewModel.kt
+++ common-app-covpass/src/main/java/de/rki/covpass/app/validitycheck/ValidityCheckViewModel.kt
@@ -12,15 +12,12 @@ import com.ibm.health.common.android.utils.BaseEvents
 import de.rki.covpass.app.dependencies.covpassDeps
 import de.rki.covpass.app.validitycheck.countries.Country
 import de.rki.covpass.app.validitycheck.countries.CountryRepository.defaultCountry
-import de.rki.covpass.logging.Lumber
 import de.rki.covpass.sdk.cert.RulesValidator
 import de.rki.covpass.sdk.dependencies.sdkDeps
 import de.rki.covpass.sdk.rules.DefaultCovPassRulesRepository
 import de.rki.covpass.sdk.rules.DefaultCovPassValueSetsRepository
 import de.rki.covpass.sdk.storage.CertRepository
 import kotlinx.coroutines.CoroutineScope
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -30,39 +27,40 @@ internal class ValidityCheckViewModel @OptIn(DependencyAccessor::class) construc
     private val certRepository: CertRepository = covpassDeps.certRepository,
     private val rulesValidator: RulesValidator = sdkDeps.rulesValidator,
     private val rulesRepository: DefaultCovPassRulesRepository = sdkDeps.rulesRepository,
-    private val valueSetsRepository: DefaultCovPassValueSetsRepository = sdkDeps.valueSetsRepository
+    private val valueSetsRepository: DefaultCovPassValueSetsRepository = sdkDeps.valueSetsRepository,
 ) : BaseReactiveState<BaseEvents>(scope) {
 
     val validationResults: MutableValueFlow<List<CertsValidationResults>> = MutableValueFlow(emptyList())
     val country: MutableValueFlow<Country> = MutableValueFlow(defaultCountry)
     val date: MutableValueFlow<LocalDateTime> = MutableValueFlow(LocalDateTime.now())
 
-    fun loadRulesAndValueSets() {
+    init {
+        loadRulesAndValidateCertificates()
+    }
+
+    fun loadRulesAndValidateCertificates() {
         launch {
-            try {
-                rulesRepository.loadRules()
-                valueSetsRepository.loadValueSets()
-            } catch (e: UnknownHostException) {
-                Lumber.e(e)
-            } catch (e: SocketTimeoutException) {
-                Lumber.e(e)
-            }
+            loadRulesAndValueSets()
+            validateCertificates()
         }
     }
 
-    fun validateCertificates() {
-        launch {
-            validationResults.value = certRepository.certs.value.certificates.map {
-                val covCertificate = it.getMainCertificate().covCertificate
-                CertsValidationResults(
+    private suspend fun loadRulesAndValueSets() {
+        rulesRepository.loadRules()
+        valueSetsRepository.loadValueSets()
+    }
+
+    private suspend fun validateCertificates() {
+        validationResults.value = certRepository.certs.value.certificates.map {
+            val covCertificate = it.getMainCertificate().covCertificate
+            CertsValidationResults(
+                covCertificate,
+                rulesValidator.validate(
                     covCertificate,
-                    rulesValidator.validate(
-                        covCertificate,
-                        country.value.countryCode.lowercase(),
-                        ZonedDateTime.of(date.value, ZoneId.systemDefault())
-                    )
+                    country.value.countryCode.lowercase(),
+                    ZonedDateTime.of(date.value, ZoneId.systemDefault())
                 )
-            }
+            )
         }
     }
 
