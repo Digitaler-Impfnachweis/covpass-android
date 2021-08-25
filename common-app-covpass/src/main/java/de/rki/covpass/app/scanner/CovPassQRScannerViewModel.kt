a7226bb65510d958b98aaa0a48106a0c5a8b443b
David Pucher <pucherd89@gmail.com>
2021-08-25 11:45:11 +0200

    Fixed import

--- common-app-covpass/src/main/java/de/rki/covpass/app/scanner/CovPassQRScannerViewModel.kt
+++ common-app-covpass/src/main/java/de/rki/covpass/app/scanner/CovPassQRScannerViewModel.kt
@@ -12,10 +12,7 @@ import com.ensody.reactivestate.getData
 import com.ibm.health.common.android.utils.BaseEvents
 import de.rki.covpass.app.dependencies.covpassDeps
 import de.rki.covpass.sdk.cert.QRCoder
-import de.rki.covpass.sdk.cert.models.CertValidationResult
-import de.rki.covpass.sdk.cert.models.CombinedCovCertificate
-import de.rki.covpass.sdk.cert.models.CovCertificate
-import de.rki.covpass.sdk.cert.models.GroupedCertificatesId
+import de.rki.covpass.sdk.cert.models.*
 import de.rki.covpass.sdk.cert.validateEntity
 import de.rki.covpass.sdk.dependencies.sdkDeps
 import de.rki.covpass.sdk.storage.CertRepository
