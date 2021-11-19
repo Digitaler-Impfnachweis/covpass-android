76253275975274af0f5be93ac9c3eb65e98dcadb
David Pucher <pucherd89@gmail.com>
2021-11-19 18:01:18 +0100

    Variable booster rule endpoint

--- covpass-sdk/src/main/java/de/rki/covpass/sdk/dependencies/SdkDependencies.kt
+++ covpass-sdk/src/main/java/de/rki/covpass/sdk/dependencies/SdkDependencies.kt
@@ -143,7 +143,7 @@ public abstract class SdkDependencies {
     }
 
     private val boosterRulesRemoteDataSource: BoosterRulesRemoteDataSource by lazy {
-        BoosterRulesRemoteDataSource(httpClient, "distribution-cff4f7147260.dcc-rules.de")
+        BoosterRulesRemoteDataSource(httpClient, dccRulesHost)
     }
 
     private val countriesRemoteDataSource: CovPassCountriesRemoteDataSource by lazy {
