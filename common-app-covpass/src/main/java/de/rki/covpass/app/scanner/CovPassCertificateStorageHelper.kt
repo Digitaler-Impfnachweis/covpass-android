abaeb76e4042290dc8789807de216815039664e2
David Pucher-external <pucherd89@gmail.com>
2021-12-16 10:49:11 +0100

    BVC-3198 - Misuse prevention (#514)
    * Misuse prevention (draft)
    
    * Katy magic
    
    * Misuse prevention (draft)
    
    * Misuse prevention (without twine fix)
    
    * Katy magic
    
    * Resolved review findings
    
    * Twine update
    
    * Adjusted misuse dialog
    
    * Minor UI changes
    
    Co-authored-by: IBM <IBM>
    
    (cherry picked from commit 224eefa58811aa7cc5269bed7b8e654bc894eee7)

---
+++ common-app-covpass/src/main/java/de/rki/covpass/app/scanner/CovPassCertificateStorageHelper.kt
@@ -0,0 +1,39 @@
+/*
+ * (C) Copyright IBM Deutschland GmbH 2021
+ * (C) Copyright IBM Corp. 2021
+ */
+
+package de.rki.covpass.app.scanner
+
+import com.ensody.reactivestate.SuspendMutableValueFlow
+import de.rki.covpass.sdk.cert.models.*
+
+public object CovPassCertificateStorageHelper {
+
+    public suspend fun addNewCertificate(
+        groupedCertificatesList: SuspendMutableValueFlow<GroupedCertificatesList>,
+        covCertificate: CovCertificate,
+        qrContent: String
+    ): GroupedCertificatesId? {
+        var certId: GroupedCertificatesId? = null
+        groupedCertificatesList.update {
+            certId = it.addNewCertificate(
+                CombinedCovCertificate(
+                    covCertificate = covCertificate,
+                    qrContent = qrContent,
+                    timestamp = System.currentTimeMillis(),
+                    status = if (covCertificate.isInExpiryPeriod()) {
+                        CertValidationResult.ExpiryPeriod
+                    } else {
+                        CertValidationResult.Valid
+                    },
+                    hasSeenBoosterNotification = false,
+                    hasSeenBoosterDetailNotification = false,
+                    hasSeenExpiryNotification = false,
+                    boosterNotificationRuleIds = mutableListOf(),
+                )
+            )
+        }
+        return certId
+    }
+}
--- common-app-covpass/src/main/java/de/rki/covpass/app/scanner/CovPassQRScannerViewModel.kt
+++ common-app-covpass/src/main/java/de/rki/covpass/app/scanner/CovPassQRScannerViewModel.kt
@@ -7,16 +7,18 @@ package de.rki.covpass.app.scanner
 
 import com.ensody.reactivestate.BaseReactiveState
 import com.ensody.reactivestate.DependencyAccessor
-import com.ensody.reactivestate.StateFlowStore
-import com.ensody.reactivestate.getData
 import com.ibm.health.common.android.utils.BaseEvents
 import de.rki.covpass.app.dependencies.covpassDeps
-import de.rki.covpass.sdk.ticketing.TicketingDataInitialization
+import de.rki.covpass.app.misuseprevention.MisusePreventionHelper
+import de.rki.covpass.app.misuseprevention.MisusePreventionStatus
 import de.rki.covpass.sdk.cert.QRCoder
-import de.rki.covpass.sdk.cert.models.*
+import de.rki.covpass.sdk.cert.models.CovCertificate
+import de.rki.covpass.sdk.cert.models.GroupedCertificates
+import de.rki.covpass.sdk.cert.models.GroupedCertificatesId
 import de.rki.covpass.sdk.cert.validateEntity
 import de.rki.covpass.sdk.dependencies.sdkDeps
 import de.rki.covpass.sdk.storage.CertRepository
+import de.rki.covpass.sdk.ticketing.TicketingDataInitialization
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.serialization.SerializationException
 
@@ -25,6 +27,7 @@ import kotlinx.serialization.SerializationException
  */
 internal interface CovPassQRScannerEvents : BaseEvents {
     fun onScanSuccess(certificateId: GroupedCertificatesId)
+    fun onLimitationWarning(qrContent: String)
     fun onTicketingQrcodeScan(ticketingDataInitialization: TicketingDataInitialization)
 }
 
@@ -33,12 +36,9 @@ internal interface CovPassQRScannerEvents : BaseEvents {
  */
 internal class CovPassQRScannerViewModel @OptIn(DependencyAccessor::class) constructor(
     scope: CoroutineScope,
-    store: StateFlowStore,
     private val qrCoder: QRCoder = sdkDeps.qrCoder,
     private val certRepository: CertRepository = covpassDeps.certRepository,
 ) : BaseReactiveState<CovPassQRScannerEvents>(scope) {
-
-    val lastCertificateId by store.getData<GroupedCertificatesId?>(null)
 
     fun onQrContentReceived(qrContent: String) {
         launch {
@@ -50,33 +50,47 @@ internal class CovPassQRScannerViewModel @OptIn(DependencyAccessor::class) const
             } catch (e: SerializationException) {
                 val covCertificate = qrCoder.decodeCovCert(qrContent)
                 validateEntity(covCertificate.dgcEntry.idWithoutPrefix)
-                val certsFlow = certRepository.certs
-                var certId: GroupedCertificatesId? = null
-                certsFlow.update {
-                    certId = it.addNewCertificate(
-                        CombinedCovCertificate(
-                            covCertificate = covCertificate,
-                            qrContent = qrContent,
-                            timestamp = System.currentTimeMillis(),
-                            status = if (covCertificate.isInExpiryPeriod()) {
-                                CertValidationResult.ExpiryPeriod
-                            } else {
-                                CertValidationResult.Valid
-                            },
-                            hasSeenBoosterNotification = false,
-                            hasSeenBoosterDetailNotification = false,
-                            hasSeenExpiryNotification = false,
-                            boosterNotificationRuleIds = mutableListOf(),
-                        )
-                    )
-                }
-                certId?.let {
-                    lastCertificateId.value = it
-                    eventNotifier {
-                        onScanSuccess(it)
-                    }
+                validateMisusePrevention(
+                    certRepository.certs.value.certificates,
+                    covCertificate,
+                    qrContent
+                )
+            }
+        }
+    }
+
+    private fun validateMisusePrevention(
+        groupedCertificates: List<GroupedCertificates>,
+        covCertificate: CovCertificate,
+        qrContent: String
+    ) {
+        when (MisusePreventionHelper.getMisusePreventionStatus(groupedCertificates, covCertificate)) {
+            MisusePreventionStatus.ALL_GOOD -> {
+                addNewCertificate(covCertificate, qrContent)
+            }
+            MisusePreventionStatus.FIRST_LIMITATION_WARNING,
+            MisusePreventionStatus.SECOND_LIMITATION_WARNING -> {
+                eventNotifier { onLimitationWarning(qrContent) }
+            }
+            MisusePreventionStatus.SAVING_BLOCKED -> {
+                throw SavingBlockedException()
+            }
+        }
+    }
+
+    private fun addNewCertificate(covCertificate: CovCertificate, qrContent: String) {
+        launch {
+            CovPassCertificateStorageHelper.addNewCertificate(
+                certRepository.certs,
+                covCertificate,
+                qrContent
+            )?.let {
+                eventNotifier {
+                    onScanSuccess(it)
                 }
             }
         }
     }
 }
+
+public class SavingBlockedException : IllegalStateException()
