489dd865ce7e6564ec8f1c7414a3e01a3fdbcffb
Edgar Oliveira-external <edgar.oliveira@pt.softinsa.com>
2022-01-03 16:47:43 +0000

    BVC-3107 Removed update info from first installation (#509)
    * Removed update info from first installation
    
    * Katy magic
    
    * Update info not in checkapp
    
    Co-authored-by: IBM <IBM>

--- common-app-covpass/src/main/java/de/rki/covpass/app/main/MainActivity.kt
+++ common-app-covpass/src/main/java/de/rki/covpass/app/main/MainActivity.kt
@@ -10,6 +10,7 @@ import de.rki.covpass.app.onboarding.WelcomeFragmentNav
 import de.rki.covpass.commonapp.BaseActivity
 import de.rki.covpass.commonapp.dependencies.commonDeps
 import de.rki.covpass.commonapp.storage.OnboardingRepository.Companion.CURRENT_DATA_PRIVACY_VERSION
+import de.rki.covpass.commonapp.updateinfo.UpdateInfoRepository.Companion.CURRENT_UPDATE_VERSION
 
 /**
  * The only Activity in the app, hosts all fragments.
@@ -19,6 +20,11 @@ internal class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        launchWhenStarted {
+            if (commonDeps.updateInfoRepository.updateInfoVersionShown.value == 0) {
+                commonDeps.updateInfoRepository.updateInfoVersionShown.set(CURRENT_UPDATE_VERSION)
+            }
+        }
         if (navigator.isEmpty() && savedInstanceState == null) {
             if (commonDeps.onboardingRepository.dataPrivacyVersionAccepted.value >= CURRENT_DATA_PRIVACY_VERSION) {
                 navigator.push(MainFragmentNav())
