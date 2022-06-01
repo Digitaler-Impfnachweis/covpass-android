1ebf4a1b9287cbc3d0a201db414bace627233f35
David Pucher-external <pucherd89@gmail.com>
2022-06-01 10:31:27 +0200

    Fix modal view header text (#692)
    (cherry picked from commit 844b01fff91faccb1927c701c39e53a2cbd2f784)

--- common-app-covpass/src/main/java/de/rki/covpass/app/certificateswitcher/CertificateSwitcherItemFragment.kt
+++ common-app-covpass/src/main/java/de/rki/covpass/app/certificateswitcher/CertificateSwitcherItemFragment.kt
@@ -69,14 +69,16 @@ internal class CertificateSwitcherItemFragment : BaseFragment() {
                 when (dgcEntry.type) {
                     VaccinationCertType.VACCINATION_FULL_PROTECTION -> {
                         val vaccination = covCertificate.dgcEntry as Vaccination
+                        val isJanssenFullProtection =
+                            vaccination.isJanssen && vaccination.doseNumber == 2
                         binding.certificateCard.createCertificateSwitcherItemView(
                             certStatus,
-                            if (vaccination.isBooster) {
+                            if (vaccination.isBooster && !isJanssenFullProtection) {
                                 getString(R.string.certificate_type_booster)
                             } else {
                                 getString(R.string.certificate_type_basic_immunisation)
                             },
-                            if (vaccination.isBooster) {
+                            if (vaccination.isBooster && !isJanssenFullProtection) {
                                 getString(
                                     R.string.certificate_timestamp_days,
                                     vaccination.occurrence?.atStartOfDay(ZoneId.systemDefault())
