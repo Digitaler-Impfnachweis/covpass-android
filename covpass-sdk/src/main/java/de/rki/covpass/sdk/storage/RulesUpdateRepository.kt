c0ae6bba0a4ca12c0134cacdb3666056f6fc50ab
David Pucher-external <pucherd89@gmail.com>
2022-09-30 12:46:08 +0200

    Update local db (#812)

--- covpass-sdk/src/main/java/de/rki/covpass/sdk/storage/RulesUpdateRepository.kt
+++ covpass-sdk/src/main/java/de/rki/covpass/sdk/storage/RulesUpdateRepository.kt
@@ -51,6 +51,6 @@ public class RulesUpdateRepository(
     }
 
     public companion object {
-        public const val CURRENT_LOCAL_DATABASE_VERSION: Int = 5
+        public const val CURRENT_LOCAL_DATABASE_VERSION: Int = 6
     }
 }
