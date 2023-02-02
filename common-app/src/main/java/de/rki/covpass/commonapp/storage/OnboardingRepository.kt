b0ba743c2119ef5a3b684aec5284ba61cd8003f3
David Pucher-external <pucherd89@gmail.com>
2023-02-02 12:45:35 +0100

    Data privacy update 1.40 (#946)

--- common-app/src/main/java/de/rki/covpass/commonapp/storage/OnboardingRepository.kt
+++ common-app/src/main/java/de/rki/covpass/commonapp/storage/OnboardingRepository.kt
@@ -17,7 +17,7 @@ public class OnboardingRepository(store: CborSharedPrefsStore) {
         store.getData("data_privacy_version_accepted", FIRST_DATA_PRIVACY_VERSION)
 
     public companion object {
-        public const val CURRENT_DATA_PRIVACY_VERSION: Int = 24
+        public const val CURRENT_DATA_PRIVACY_VERSION: Int = 25
         public const val FIRST_DATA_PRIVACY_VERSION: Int = 0
     }
 }
